d668cbaf34a9fb14669c4b67123461f4429fae19
Valentin Radev <valio_ch@mail.bg>
2016-06-26 22:54:12 +0300

    Added production mongodb configuration.

--- server/config/config.js
+++ server/config/config.js
@@ -6,5 +6,10 @@ module.exports = {
         rootPath: rootPath,
         db: 'mongodb://localhost:27017/wbta',
         port: process.env.PORT || 3000
+    },
+    production: {
+        rootPath: rootPath,
+        db: 'mongodb://Vali0:asd123@ds021884.mlab.com:21884/wbta',
+        port: process.env.PORT || 3030
     }
 };
